cc21da3debdab503a2ba329e79c3821f1715905f
Furkan Masraf <masraffurkan@gmail.com>
2025-08-18 16:47:06 +0300

    changeIncome1

--- app/Tabs/Gelirler.tsx
+++ app/Tabs/Gelirler.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Income() {
   return (
-    <View>
-      <Text>Income Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>💵 Gelirler</Text>
+      <Text style={styles.subtitle}>Burada gelir kayıtlarını göreceksin.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F9FAFB",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#007BFF",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "gray",
+    marginTop: 8,
+  },
+});
